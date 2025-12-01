8e065d0284755cd34f0c9e86c76661df323864e6
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-12-01 22:09:32 +0530

    mutlitple route handler

--- src/app.js
+++ src/app.js
@@ -8,32 +8,45 @@ app.use(express.json());
 //REQUEST HANDLER
 
 //READING QUERY PARAMS IN THE ROUTES
-app.use("/user/:userId/:name",(req,res)=>{
-    console.log(req.params);//IT WILL READ THE DATA FROM URL AFTER /USER
-});
+// app.use("/user/:userId/:name",(req,res)=>{
+//     console.log(req.params);//IT WILL READ THE DATA FROM URL AFTER /USER
+// });
 
 //This will only handle GET call to /user
-app.get("/user",(req,res)=>{
-    res.send({firstName: "Rudra", lastName:"Jat"});
-});
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: "Rudra", lastName:"Jat"});
+// });
 
-app.post("/user",(req,res)=>{
-    console.log(req.body);//This prints the data sent by the user in the terminal.
-    //saving data to DB
-    res.send("Data successfully saved to the DB!");
-});
+// app.post("/user",(req,res)=>{
+//     console.log(req.body);//This prints the data sent by the user in the terminal.
+//     //saving data to DB
+//     res.send("Data successfully saved to the DB!");
+// });
 
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfully!");
-});
+// app.delete("/user",(req,res)=>{
+//     res.send("Deleted Successfully!");
+// });
 
-//this will match all the HTTP method API calls to /test 
-app.use("/test",(req,res)=>{
-    res.send("hello from my server");
-})
-app.use("/home",(req,res)=>{
-    res.send("hello from dashboard");
-})
+// //this will match all the HTTP method API calls to /test 
+// app.use("/test",(req,res)=>{
+//     res.send("hello from my server");
+// })
+// app.use("/home",(req,res)=>{
+//     res.send("hello from dashboard");
+// })
+
+
+//ROUTE HANDDLER CHAINING
+app.use("/user",(req,res,next)=>{
+    // res.send("1st response");
+    console.log("This is response from first route handler");
+    next();
+},
+    (req,res)=>{
+        res.send("2nd response");
+        console.log("This is response from second route handler");
+    }
+)
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...")
