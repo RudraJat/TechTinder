338aa2dfb4f8911645de1393e8c63bd73235a8dc
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-12-09 23:23:08 +0530

    sanitize the schema for DB

--- src/model/user.js
+++ src/model/user.js
@@ -3,22 +3,37 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        minlength: 4,
+        maxlength: 20,
+        required: true,
     },
     lastName: {
         type: String,
+        minlength: 4,
+        maxlength: 20,
     },
     email: {
         type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
     },
     password: {
         type: String,
     },
     age: {
         type: Number,   
+        min: 13,
     },
     gender: {
         type: String,
     },
+    bio: {
+        type: String,
+        maxlength: 500,
+        default: "This user prefers to keep an air of mystery about them.",
+    }, 
 });
 
 const User = mongoose.model("User", userSchema);
