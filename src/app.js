47f0d3f76fd2649d4266a675c390be66a46d0d7f
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-12-11 23:00:44 +0530

    encrypted the password before adding it in the database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const connectDB = require('./config/database.js');
-const User = require('./model/user.js');
-const {validateSignupData }= require('./utils/validate.js');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const connectDB = require("./config/database.js");
+const User = require("./model/user.js");
+const { validateSignupData } = require("./utils/validate.js");
 
 const app = express();
 
@@ -26,105 +26,104 @@ const app = express();
 //parsing json data from request bodY
 app.use(express.json());
 
-app.post("/signup", async(req, res)=>{
-    //validating signup data
-    try{
-        validateSignupData(req);
+app.post("/signup", async (req, res) => {
+  //validating signup data
+  try {
+    validateSignupData(req);
 
     // Encrypt the password before saving to database
-    const {password} = req.body;
+    const { firstName, lastName, email, password, skills } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10); //10 is the salt rounds- the higher the rounds, the more secure but slower
     console.log("Hashed Password:", hashedPassword);
     //Creating a new instance of User model
-    const users = new User(req.body);
+    const users = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      skills,
+    });
 
-    
-        await users.save();
-        res.send("User signed up successfully!");
-    }catch(err){
-        res.status(201).send("Error signing up user."+err.message);
-    }
+    await users.save();
+    res.send("User signed up successfully!");
+  } catch (err) {
+    res.status(201).send("Error signing up user." + err.message);
+  }
 });
 
 //GET USER BY EMAIL
-app.get("/user", async(req, res)=>{
-    const userEmail = req.body.email;
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
 
-    try{
-        const user = await User.findOne({email: userEmail});
-        if(!user){
-            res.status(404).send("User not found");
-        }else{
-            res.send(user);
-        }
-    }catch(err){
-        res.status(500).send("Error retrieving user. "+err.message);
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
     }
-})
+  } catch (err) {
+    res.status(500).send("Error retrieving user. " + err.message);
+  }
+});
 
 //FEED api- GET /feed - get all users from the database
-app.get("/feed", async(req, res)=>{
-
-    try{
-        const users = await User.find({});
-        if(!users){
-            res.status(404).send("No users found");
-        }else{
-            res.send(users);
-        }
-    }catch(err){
-        res.status(500).send("Error retrieving users. "+err.message);
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(users);
     }
-})
+  } catch (err) {
+    res.status(500).send("Error retrieving users. " + err.message);
+  }
+});
 
 //DELETE user by id
-app.delete("/user", async(req, res)=>{
-    const userId = req.body.userId;
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
 
-    try{
-        const deleteUser = await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully!");
-    }catch(err){
-        res.status(500).send("Error deleting user. "+err.message);
-    }
-})
-
+  try {
+    const deleteUser = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully!");
+  } catch (err) {
+    res.status(500).send("Error deleting user. " + err.message);
+  }
+});
 
 //Update data of user
-app.patch("/user", async(req, res)=>{
-    const userId = req.body.userId;
-    const data = req.body;
-    try{
-        const allowedUpdates = ["userId", "gender", "about", "skills"];
-        const isUpdateAllowed = Object.keys(data).every((k)=> 
-            allowedUpdates.includes(k)
-        );
-        if(!isUpdateAllowed){
-            throw new Error("Update not allowed")
-        }
-
-        
-        const user = await User.findByIdAndUpdate({ _id : userId}, data, {
-            returnDocument: 'after', //this will return value of updated document
-            runValidators: true //this will run the validators defined in the schema
-        });
-        // console.log(user);
-        res.send("User updated successfully!");
-    }catch(err){
-        res.status(500).send("Error updating user. "+err.message);
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const allowedUpdates = ["userId", "gender", "about", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      allowedUpdates.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
     }
-})
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after", //this will return value of updated document
+      runValidators: true, //this will run the validators defined in the schema
+    });
+    // console.log(user);
+    res.send("User updated successfully!");
+  } catch (err) {
+    res.status(500).send("Error updating user. " + err.message);
+  }
+});
 
 connectDB()
-    .then(()=>{
-        console.log("Database connected successfully!");
-        app.listen(1111, () => {
-            console.log("Server is listening on port 1111");    
-        });
-    })
-    .catch((err)=>{
-        console.log("Database connection failed!", err);
+  .then(() => {
+    console.log("Database connected successfully!");
+    app.listen(1111, () => {
+      console.log("Server is listening on port 1111");
     });
-
-
-
+  })
+  .catch((err) => {
+    console.log("Database connection failed!", err);
+  });
